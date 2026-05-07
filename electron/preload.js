2a2ec7926a2e4b7ac9e3a673313fc5e2faa488be
ibrah555 <ibrah555@users.noreply.github.com>
2026-05-07 10:01:00 +0300

    chore: Vercel deployment setup and cleanup

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    // We can add IPC methods here if needed later
-    platform: process.platform
-});
